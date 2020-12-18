b99ad09361bd530de6f4bdef173850f3eef270f7
ubuntu <ubuntu@localhost.localdomain>
2020-12-18 23:32:36 +0800

    update

---
+++ EBAZ4205/4203/gpio_test/gpio_test.sdk/gpio_test/Debug/src/helloworld.d
@@ -0,0 +1,54 @@
+src/helloworld.o src/helloworld.o: ../src/helloworld.c ../src/platform.h \
+ ../src/platform_config.h \
+ ../../fsbl_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../fsbl_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../fsbl_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../fsbl_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../fsbl_bsp/ps7_cortexa9_0/include/bspconfig.h \
+ ../../fsbl_bsp/ps7_cortexa9_0/include/xgpiops.h \
+ ../../fsbl_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../fsbl_bsp/ps7_cortexa9_0/include/xil_assert.h \
+ ../../fsbl_bsp/ps7_cortexa9_0/include/xgpiops_hw.h \
+ ../../fsbl_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../fsbl_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../fsbl_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../fsbl_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../fsbl_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../fsbl_bsp/ps7_cortexa9_0/include/xplatform_info.h \
+ ../../fsbl_bsp/ps7_cortexa9_0/include/sleep.h
+
+../src/platform.h:
+
+../src/platform_config.h:
+
+../../fsbl_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../fsbl_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../fsbl_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../fsbl_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../fsbl_bsp/ps7_cortexa9_0/include/bspconfig.h:
+
+../../fsbl_bsp/ps7_cortexa9_0/include/xgpiops.h:
+
+../../fsbl_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../fsbl_bsp/ps7_cortexa9_0/include/xil_assert.h:
+
+../../fsbl_bsp/ps7_cortexa9_0/include/xgpiops_hw.h:
+
+../../fsbl_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../fsbl_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../fsbl_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../fsbl_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../fsbl_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../fsbl_bsp/ps7_cortexa9_0/include/xplatform_info.h:
+
+../../fsbl_bsp/ps7_cortexa9_0/include/sleep.h:
---
+++ EBAZ4205/4203/gpio_test/gpio_test.srcs/constrs_1/new/led.xdc
@@ -0,0 +1,6 @@
+
+
+set_property IOSTANDARD LVCMOS33 [get_ports {GPIO_0_tri_io[1]}]
+set_property IOSTANDARD LVCMOS33 [get_ports {GPIO_0_tri_io[0]}]
+set_property PACKAGE_PIN W14 [get_ports {GPIO_0_tri_io[1]}]
+set_property PACKAGE_PIN W13 [get_ports {GPIO_0_tri_io[0]}]
